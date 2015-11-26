cf72d2c9e20b300ed109a1b189ffc940e4aae72e
daihuiming <daihuiming@baidu.com>
2015-11-26 14:53:35 +0800

    update cache stragety

--- src/store/cache.js
+++ src/store/cache.js
@@ -17,7 +17,7 @@ function Cache() {
 
     this.items = {};
 
-    this._maxMinute = 2;
+    this._maxMinute = 4;
 };
 
 /**
@@ -40,7 +40,7 @@ Cache.prototype.add = function (key, item) {
                 me.items[key][subKey] = me.items[key][subKey].concat(item[subKey]);
             }
             else {
-                me.items[key][subKey] = item[subKey] || [];
+                me.items[key][subKey] = item[subKey];
             }
         });
     }
@@ -57,26 +57,28 @@ Cache.prototype.get = function (key) {
     var existItem = me.items[key];
 
     // 延迟2分钟
-    var before = Date.now() - me._maxMinute * 60 * 1000;
+    var after = Date.now() + me._maxMinute * 60 * 1000;
 
     // 如果发现有数据就进行处理，如果发现整个单项缓存都没有数据了就关掉
     if (existItem && Object.keys(existItem).length) {
         Object.keys(existItem).forEach(function (subKey) {
 
-            var currentValue = existItem.subKey;
+            var currentValue = existItem[subKey];
 
             // 如果有值了我们就进行比较，我们能容忍的误差在5分钟以内，否则删除，不然会内存泄露
-            if (currentValue.now) {
-                // 如果已经超过2分钟了，我们就认为可以了
-                if (currentValue.now >= before) {
-                    result[key][subKey] = currentValue;
-                    delete existItem[subKey];
+            if (currentValue) {
+                if (currentValue.now) {
+                    // 如果已经超过2分钟了，我们就认为可以了
+                    if (currentValue.now >= Date.now()) {
+                        result[key][subKey] = currentValue;
+                        delete existItem[subKey];
+                    }
                 }
-            }
-            else {
-                existItem.subKey.now = Date.now();
-            }
-        }
+                else {
+                    existItem[subKey].now = after;
+                }
+            }   
+        });
     }
     else {
         delete me.items[key];
